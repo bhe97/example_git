c6aeefa5de05cacd4c58a3defded2423d6d5bb76
Alberto Huerta <j.betito.h.e@gmail.com>
2020-06-10 22:39:30 -0500

    [wip]
    *MERGE WITH STASH

--- App.js
+++ App.js
@@ -47,7 +47,7 @@ const App: () => React$Node = () => {
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes hnijmo0e </Text>
+              <Text style={styles.sectionTitle}>See Your Changes hnijmo0e vamos a bailar</Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
